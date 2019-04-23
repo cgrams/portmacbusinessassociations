55e572d001ffc02069bd5e8bf0ab797eb12ed9aa
Caleb Grams <cmgrams@gmail.com>
2019-04-23 23:54:50 +1000

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss', './mainMobile.scss']
+  styleUrls: ['./app.component.scss', './mainMobile.scss'],
+
+  animations: [
+	  trigger('changeDivSize', [
+	    state('initial', style({
+ 
+	      zIndex: '5',
+	      position: 'relative',
+    	  backgroundColor: 'red',
+    gridRowStart: '1',
+    gridRowEnd: '7',
+    gridColumnStart: '2',
+    gridColumnEnd: '4',
+    right: '2000px'
+
+
+
+	    })),
+	    state('final', style({
+ 
+	      zIndex: '5',
+	      position: 'relative',
+    	  backgroundColor: '#f19764',
+    gridRowStart: '1',
+    gridRowEnd: '7',
+    gridColumnStart: '2',
+    gridColumnEnd: '4',
+    right: '0px'
+
+
+
+
+	    })),
+	    transition('initial=>final', animate('1500ms')),
+	    transition('final=>initial', animate('1000ms'))
+	  ]),
+  ]
+
+
 })
 export class AppComponent {
   title = 'portmacbusinessassociations';
@@ -13,10 +51,11 @@ export class AppComponent {
   opened: boolean;
 
 
-clickMessage = '';
+currentState = 'initial';
 
-  onClickMe() {
-    this.clickMessage = 'You are my hero!';
-    alert();
-  }
+changeState() {
+  this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
+}
+
+
 }
