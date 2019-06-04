6c696ba70bd91d888800db42190fa28e1222a3b2
Caleb Grams <cmgrams@gmail.com>
2019-06-04 21:31:40 +1000

    f

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+	apiKey: "AIzaSyCaGBqe_6EuOU3TNzo_FMOX8D-UITf26lw",
+    authDomain: "portmacbusinessassociations.firebaseapp.com",
+    databaseURL: "https://portmacbusinessassociations.firebaseio.com",
+    projectId: "portmacbusinessassociations",
+    storageBucket: "portmacbusinessassociations.appspot.com",
+    messagingSenderId: "210855222436"
+  }
+};
